f08c4030e87bd4ca2e40c3c47e16fd3a60c2c70a
Bijou <bijou.raj12@gmail.com>
2024-05-05 17:40:43 -0700

    Printed df so all features are printed, as well as created new dataframes without DC and US, respectively

--- cleanedData.py
+++ cleanedData.py
@@ -26,4 +26,12 @@ print(null_values)
 
 df['State'] = df['State'].str.strip()
 
-print(df)
+print(df[['State', 'Uninsured Rate (2010)', 'Uninsured Rate (2015)', 'Uninsured Rate Change (2010-2015)', 'Health Insurance Coverage Change (2010-2015)']])
+print(df[['Employer Health Insurance Coverage (2015)', 'Marketplace Health Insurance Coverage (2016)', 'Marketplace Tax Credits (2016)', 'Average Monthly Tax Credit (2016)']])
+print(df[['State Medicaid Expansion (2016)', 'Medicaid Enrollment (2013)', 'Medicaid Enrollment (2016)', 'Medicaid Enrollment Change (2013-2016)', 'Medicare Enrollment (2016)']])
+
+df_noDC = df[df['State'] != 'District of Columbia'].reset_index(drop=True)
+print(df_noDC)
+
+df_noUS = df_noDC[:-1]
+print(df_noUS)
